2b09a801bee2089559705d68fb174072e3f4d508
zhoujason <7582985009@qq.com>
2022-11-03 19:39:22 +0800

    Initial commit

---
+++ 温度转换.c
@@ -0,0 +1,24 @@
+//
+// Created by Zhou Jason on 2022/11/3.
+//
+
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+{
+    float fahrenheit, celsius;
+
+    printf("请输入华氏温度： ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+
+    printf("摄氏度为: %.1f\n", celsius);
+
+    return 0;
+
+}
